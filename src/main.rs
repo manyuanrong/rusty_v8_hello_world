fe259a75eeaab4e0166541a81e2f6beb31ec1abb
Enok <416828041@qq.com>
2019-12-27 23:44:51 +0800

    Bump to 0.0.16

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use rusty_v8::{Isolate, V8, Allocator, HandleScope, Script, Context, Locker, Local};
-use rusty_v8::platform;
+use rusty_v8::{Isolate, V8, HandleScope, Script, Context, Locker, Local};
+use rusty_v8::{platform, new_default_allocator};
 
 fn main() {
     let platform = platform::new_default_platform();
@@ -7,31 +7,28 @@ fn main() {
     V8::initialize();
 
     let mut create_params = Isolate::create_params();
-    create_params.set_array_buffer_allocator(Allocator::new_default_allocator());
+    create_params.set_array_buffer_allocator(new_default_allocator());
     let isolate = Isolate::new(create_params);
     let mut locker = Locker::new(&isolate);
 
-    HandleScope::enter(&mut locker, |scope| {
+    {
+        let mut handle_scope = HandleScope::new(&mut locker);
+        let scope = handle_scope.enter();
+
         let mut context = Context::new(scope);
         context.enter();
         let code = rusty_v8::String::new(scope, "'Hello World!'").unwrap();
         code.to_rust_string_lossy(scope);
         let mut script = Script::compile(scope, context, code, None).unwrap();
         let result = script.run(scope, context).unwrap();
-        let result: Local<rusty_v8::String> = cast(result);
+        let result: Local<rusty_v8::String> = unsafe { std::mem::transmute_copy(&result) };
 
         let str = result.to_rust_string_lossy(scope);
 
         println!("{}", str);
 
         context.exit();
-    });
+    }
 
     drop(locker);
-}
-
-// TODO Safer casts https://github.com/denoland/rusty_v8/issues/51
-fn cast<U, T>(local: Local<T>) -> Local<U> {
-    let cast_local: Local<U> = unsafe { std::mem::transmute_copy(&local) };
-    cast_local
 }
